98b13220b25c83320c506c30f641b2810ac97a6a
Pitra <justin.pitra@gmail.com>
2024-10-07 15:29:33 +0200

    change type for %money

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 class VendorMachine
 {
     private bool $isOn;
     private int $snacksQty;
-    private float $money;
+    private int $money;
 
     public function __construct()
     {
