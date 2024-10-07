72d05370751857e65059a9e7c35dc7d2fef51165
Pitra <justin.pitra@gmail.com>
2024-10-07 16:25:59 +0200

    add 2 rules

--- order.php
+++ order.php
@@ -2,38 +2,40 @@
 
 class Order {
 
+    private array $products;
+    private string $customerName;
+    private float $totalPrice;
+    private int $id;
+    private DateTime $createdAt;
+    private string $status;
+    private ?string $shippingMethod;
+    private ?string $shippingAddress;
 
-	private array $products;
+    public function __construct(string $customerName, array $products) {
+        $this->status = "CART";
+        $this->createdAt = new DateTime();
+        $this->id = rand();
 
-	private string $customerName;
+        $this->products = $products;
+        $this->customerName = $customerName;
+        $this->totalPrice = count($products) * 5;
 
-	private float $totalPrice;
+        echo "Commande {$this->id} créée, d'un montant de {$this->totalPrice} !<br>";
+    }
 
-	private int $id;
+    public function maxQty(): void {
+        if (count($this->products) > 5) {
+            echo "Vous dépassez la limite de 5 produits<br>";
+        }
+    }
 
-	private DateTime $createdAt;
-
-	private string $status;
-
-	private ?string $shippingMethod;
-
-	private ?string $shippingAddress;
-
-
-	public function __construct(string $customerName, array $products) {
-		$this->status = "CART";
-		$this->createdAt = new DateTime();
-		$this->id = rand();
-
-		$this->products = $products;
-		$this->customerName = $customerName;
-		$this->totalPrice = count($products) * 5;
-
-		echo "Commande {$this->id} créée, d'un montant de {$this->totalPrice} !";
-	}
-
+    public function blackList(): void {
+        if ($this->customerName === "David Robert") {
+            echo "Vous êtes banni de ce site<br>";
+        }
+    }
 }
 
 $order = new Order('David Robert', ['Casque', 'Téléphone']);
-
-
+$order->maxQty();
+$order->blackList();
